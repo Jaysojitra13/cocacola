1a0388bc723df24e0cb36034110bffe7d49c4456
Jay Sojitra <jaysojitra13@gmail.com>
2022-04-06 11:02:14 +0530

    Update test1.js

--- assets/js/test1.js
+++ assets/js/test1.js
@@ -1,1 +1,16 @@
-console.log("Heelo there");
+window.addEventListener('message', function (eventData) {
+  console.log("CHECKING FOR EVENT : SHIV");
+  console.log(eventData);
+  try {
+      console.log("1 => ", eventData);
+      console.log("1.1 => ", JSON.parse(eventData.data));
+      
+      if (JSON.parse(eventData.data)) {
+          let event = JSON.parse(eventData.data);
+          console.log(event)
+      }
+
+  } catch (error) {
+      return;
+  }
+}, false);
